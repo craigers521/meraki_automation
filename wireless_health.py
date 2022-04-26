882aa11a6ebc6894d149fd57d048dd185663a14b
craigers521 <craigers521@gmail.com>
2022-04-26 13:11:47 -0500

    error handling, formatting

--- wireless_health.py
+++ wireless_health.py
@@ -3,12 +3,17 @@ import os
 import json
 
 api_key = os.getenv("MERAKI_API")
-org_id = "1160673"
+org_id = "398020"
+
 
 def get_networks():
     networks = dash.organizations.getOrganizationNetworks(org_id, total_pages="all")
-    network_ids = [network["id"] for network in networks]
+    # only store networkIds for combined or wireless networks
+    network_ids = [
+        network["id"] for network in networks if "wireless" in network["productTypes"]
+    ]
     return network_ids
+
 
 def init_health(network_ids):
     health_dict = {}
@@ -16,14 +21,20 @@ def init_health(network_ids):
         health_dict[network] = {"health": 100, "status": "healthy"}
     return health_dict
 
+
 def check_device(update_dict):
+    # optimize: make less API calls if made single org call then mapped device status to networks
     for network in update_dict:
-        statuses = dash.organizations.getOrganizationDevicesStatuses(org_id, total_pages="all", networkIds=[network])
-        bad_count = status_counter(statuses)
-        if bad_count > 0:
-            update_dict[network]["health"] -= bad_count
-            update_dict[network]["status"] = "APs offline or alerting"
+        statuses = dash.organizations.getOrganizationDevicesStatuses(
+            org_id, total_pages="all", networkIds=[network]
+        )
+        if len(statuses) > 0:  # check to see if network has devices
+            bad_count = status_counter(statuses)
+            if bad_count > 0:
+                update_dict[network]["health"] -= bad_count
+                update_dict[network]["status"] = "APs offline or alerting"
     return update_dict
+
 
 def status_counter(statuses):
     bad_count = 0
@@ -33,21 +44,27 @@ def status_counter(statuses):
     bad_avg = (bad_count / len(statuses)) * 100
     return bad_avg
 
+
 def check_connstats(update_dict):
     for network in update_dict:
-        connstats = dash.wireless.getNetworkWirelessConnectionStats(network, timespan=600)
+        connstats = dash.wireless.getNetworkWirelessConnectionStats(
+            network, timespan=600
+        )
         conn_score = conn_counter(connstats)
         if conn_score["score"] > 0:
             update_dict[network]["health"] -= conn_score["score"]
             update_dict[network]["status"] = conn_score["reason"]
     return update_dict
 
+
 def conn_counter(connstats):
-    total_inter = connstats["auth"] + connstats["dhcp"] + connstats["dns"] + connstats["success"]
+    total_inter = (
+        connstats["auth"] + connstats["dhcp"] + connstats["dns"] + connstats["success"]
+    )
     conn_score = {"score": 0, "reason": ""}
     if total_inter > 0:
         if connstats["auth"] > 0:
-            #need to figure out how to weight score decrementer for situations with lots of failures and/or no successes
+            # need to figure out how to weight score decrementer for situations with lots of failures and/or no successes
             score_dec = (connstats["auth"] / total_inter) * 100
             conn_score["score"] = score_dec
             conn_score["reason"] += "Authentication Failures;"
@@ -62,7 +79,6 @@ def conn_counter(connstats):
     return conn_score
 
 
-
 def main():
 
     network_ids = get_networks()
@@ -72,8 +88,7 @@ def main():
     print(json.dumps(health_dict, indent=2))
 
 
-
 if __name__ == "__main__":
-    dash = meraki.DashboardAPI(api_key, suppress_logging=True)
 
+    dash = meraki.DashboardAPI(api_key, suppress_logging=True)
     main()
