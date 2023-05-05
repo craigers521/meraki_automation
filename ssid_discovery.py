9ea1681e28b02832f46945b75689a111005d2a59
craigers521 <craigers521@gmail.com>
2023-05-05 15:59:45 -0500

    ssid search script

---
+++ ssid_discovery.py
@@ -0,0 +1,34 @@
+import meraki
+import os
+
+
+api_key = os.getenv("MERAKI_KEY")
+org_id = os.getenv("ORG_ID")
+target_ssid = "craig-lab-meraki"
+
+def getNetworks():
+    networks = dash.organizations.getOrganizationNetworks(org_id, total_pages=all)
+    wireless_nets = [
+        network for network in networks if "wireless" in network["productTypes"]
+    ]
+    return wireless_nets
+
+def findSsidEnabled(networks):
+    enabled_nets = []
+    for network in networks:
+        ssids = dash.wireless.getNetworkWirelessSsids(network['id'])
+        for ssid in ssids:
+            if ssid['name'] == target_ssid and ssid['enabled']:
+                enabled_nets.append(network)
+    return enabled_nets
+
+def output_names(networks):
+    for network in networks:
+        print(network['name'])
+    pass
+
+if __name__ == "__main__":
+    dash = meraki.DashboardAPI(api_key, suppress_logging=True)
+    networks = getNetworks()
+    enabled_nets = findSsidEnabled(networks)
+    output_names(enabled_nets)
