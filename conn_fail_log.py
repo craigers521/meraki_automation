72070c2202a9d7abf2dee5aa314834c380505161
Craig Egan <craigers521@gmail.com>
2024-06-13 15:13:12 -0500

    removing unused function

--- conn_fail_log.py
+++ conn_fail_log.py
@@ -1,6 +1,5 @@
 import meraki
 import os
-import sys
 import csv
 from datetime import datetime
 
@@ -8,26 +7,10 @@ api_key = os.getenv("MERAKI_KEY")
 org_id = "1280364"
 
 
-
 def get_networks():
     networks = dash.organizations.getOrganizationNetworks(org_id, total_pages="all")
     network_ids = [network["id"] for network in networks if "wireless" in network["productTypes"]]
     return network_ids
-
-
-def find_clients(networks, filter_mac):
-    found_clients = []
-    for network in networks:
-        client_list = dash.networks.getNetworkClients(network, mac=filter_mac)
-        if len(client_list) > 0:
-            client_info = [
-                {k: d[k] for k in ("id", "mac", "ip", "switchport")}
-                for d in client_list
-            ]
-            for device in client_info:
-                device["network"] = network
-                found_clients.append(device)
-    return found_clients
 
 
 def get_failed(networks):
@@ -55,5 +38,5 @@ if __name__ == "__main__":
     dash = meraki.DashboardAPI(api_key, suppress_logging=True)
     network_ids = get_networks()
     conn_logs = get_failed(network_ids)
-    print(conn_logs)
+    #print(conn_logs)
     output_csv(conn_logs)
